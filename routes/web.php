ab23c23e06f56ff1a29446efba250c8d8a7821ce
julio-2001 <santos2001martins@gmail.com>
2023-10-31 21:16:36 -0300

    feat(Auth): add laravel/ui for auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => "web"],function(){
+    Route::get('/',[HomeController::class, "index"]);
+
+    Auth::routes();
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+Route::group(['middleware' => 'auth'],function(){
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "!./resources/views/layouts/app.blade.php",
     ],
     theme: {
         extend: {},
